590d77814afe0518729640cab5f4b908f80a16ec
hmackowski <hmackowski@gmail.com>
2023-09-11 23:28:21 -0400

    modified pom.xml and other files

--- src/main/java/com/launch/trainee/LaunchTraineeApplication.java
+++ src/main/java/com/launch/trainee/LaunchTraineeApplication.java
@@ -3,7 +3,7 @@ package com.launch.trainee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {ComponentScanMarker.class})
 public class LaunchTraineeApplication {
 
 	public static void main(String[] args) {
