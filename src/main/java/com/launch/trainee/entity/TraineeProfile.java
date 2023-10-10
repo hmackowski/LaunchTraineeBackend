3e621c2bb45b8ccbf2054dcb00923faf7ca2a494
hmackowski <hmackowski@gmail.com>
2023-10-10 01:33:25 -0400

    Removed unsed entities

--- src/main/java/com/launch/trainee/entity/TraineeProfile.java
+++ src/main/java/com/launch/trainee/entity/TraineeProfile.java
@@ -9,9 +9,5 @@ import lombok.Data;
 @Table(name = "traineeProfile")
 public class TraineeProfile {
   private Trainee trainee;
-  private ClassroomKnowledge classroomKnowledge;
-  private PracticalSkills practicalSkills;
-  private SoftSkills softSkills;
-  private OverallRating overallRating;
   private List<Attendance> attendanceList;
 }
